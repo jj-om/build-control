ecc02568b947f9ce92f7b8a01541ea382f480019
alegarciap <alegarpre@gmail.com>
2025-03-19 19:17:28 -0600

    DTO para probar

---
+++ dto_negocios/src/main/java/dto/ActividadDTO.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+/**
+ *
+ * @author alega
+ */
+public class ActividadDTO {
+    
+    private String tituloActividad;
+    private String descripcionActividad;
+
+    public ActividadDTO() {
+    }
+
+    public ActividadDTO(String tituloActividad, String descripcionActividad) {
+        this.tituloActividad = tituloActividad;
+        this.descripcionActividad = descripcionActividad;
+    }
+
+    public String getTituloActividad() {
+        return tituloActividad;
+    }
+
+    public void setTituloActividad(String tituloActividad) {
+        this.tituloActividad = tituloActividad;
+    }
+
+    public String getDescripcionActividad() {
+        return descripcionActividad;
+    }
+
+    public void setDescripcionActividad(String descripcionActividad) {
+        this.descripcionActividad = descripcionActividad;
+    }
+    
+    
+    
+}
