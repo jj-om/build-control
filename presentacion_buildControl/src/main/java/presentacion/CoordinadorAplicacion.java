39b948e283fd8d03a140d0946cef08f0fb5a1dc4
alegarciap <alegarpre@gmail.com>
2025-03-19 19:19:37 -0600

    Creación básica de pantallas y creación del coordinador aplicación

---
+++ presentacion_buildControl/src/main/java/presentacion/CoordinadorAplicacion.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package presentacion;
+
+/**
+ *
+ * @author alega
+ */
+public class CoordinadorAplicacion {
+    
+    // Hacer una instancia ÚNICA 
+    private static CoordinadorAplicacion coordinador;
+    
+    // Forms
+    private ObraSeleccionadaForm obraSeleccionadaForm;
+    private ActividadesForm actividadesForm;
+    private MaterialesForm materialesForm;
+    private HerramientasYMaquinariaForm hymForm;
+    private PersonalForm personalForm;
+
+    public CoordinadorAplicacion() {
+    }
+    
+    // Método para obtener una única instancia
+    public static CoordinadorAplicacion getInstancia() {
+        if (coordinador == null) {
+            coordinador = new CoordinadorAplicacion();
+        }
+        return coordinador;
+    }
+    
+    // Métodos para mostrar las pantallas
+    public void mostrarObraSeleccionada() {
+        if (obraSeleccionadaForm == null) {
+            obraSeleccionadaForm = new ObraSeleccionadaForm(this);
+        }
+        obraSeleccionadaForm.setVisible(true);
+    }
+
+    public void mostrarActividades() {
+        if (actividadesForm == null) {
+            actividadesForm = new ActividadesForm(this);
+        }
+        actividadesForm.setVisible(true);
+    }
+
+    public void mostrarMateriales() {
+        if (materialesForm == null) {
+            materialesForm = new MaterialesForm(this);
+        }
+        materialesForm.setVisible(true);
+    }
+
+    public void mostrarHerramientasYMaquinaria() {
+        if (hymForm == null) {
+            hymForm = new HerramientasYMaquinariaForm(this);
+        }
+        hymForm.setVisible(true);
+    }
+
+    public void mostrarPersonal() {
+        if (personalForm == null) {
+            personalForm = new PersonalForm(this);
+        }
+        personalForm.setVisible(true);
+    }
+    
+}
--- presentacion_buildControl/src/main/java/presentacion/Pruebas.java
+++ presentacion_buildControl/src/main/java/presentacion/Pruebas.java
@@ -11,6 +11,6 @@ package presentacion;
 public class Pruebas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CoordinadorAplicacion.getInstancia().mostrarObraSeleccionada();
     }
 }
